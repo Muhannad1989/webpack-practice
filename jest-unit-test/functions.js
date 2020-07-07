cf9d8ab96441907aee0e65f32fd7167e8d243b91
muhannad <muhannadjudah@gmail.com>
2020-07-07 18:15:03 +0200

    finish jest test

--- jest-unit-test/functions.js
+++ jest-unit-test/functions.js
@@ -1,5 +1,59 @@
+const axios = require("axios");
 const functions = {
-  add: (num1, num2) => num1 + num2
+  add: (num1, num2) => num1 + num2,
+  // *****************************
+
+  isNull: () => null,
+  // *****************************
+
+  checkValue: (x) => x,
+  // *****************************
+
+  createUser: () => {
+    const user = { firstName: "Muhannad" };
+    user["lastName"] = "Judah";
+    return user;
+  },
+  // *****************************
+
+  fetchUser: async () => {
+    try {
+      const user = await axios.get("https://jsonplaceholder.typicode.com/users/1");
+      const res = user.data;
+      return res;
+    } catch (err) {
+      return { error: err };
+    }
+  },
+
+  // *****************************
+  reversString: (text) => {
+    return text.toLowerCase().split("").reverse().join("");
+  },
+
+  // *****************************
+  chunkArray: (arr, len) => {
+    const chunkedArr = [];
+    arr.forEach((item) => {
+      // grab last item in array
+      const last = chunkedArr[chunkedArr.length - 1];
+      if (!last || last.length === len) {
+        chunkedArr.push([item]);
+      } else {
+        last.push(item);
+      }
+    });
+    return chunkedArr;
+  },
+  // *****************************
+
+  isAnagram: (str1, str2) => {
+    // internal func
+    const formaStr = (text) => {
+      return text.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
+    };
+    return formaStr(str1) === formaStr(str2);
+  }
 };
 
 module.exports = functions;
--- jest-unit-test/functions.test.js
+++ jest-unit-test/functions.test.js
@@ -1,5 +1,76 @@
 const functions = require("./functions");
 
-test("add tow numbers", () => {
-  expect(functions.add(4, 4)).toBe(8);
+const start = () => console.log("start test");
+const end = () => console.log("end test");
+
+beforeAll(start);
+afterAll(end);
+
+test("calculate tow numbers", () => {
+  expect(functions.add(4, 4)).not.toBe(7);
+});
+
+test("is it null", () => {
+  expect(functions.isNull()).toBe(null);
+});
+
+test("is it falsy", () => {
+  expect(functions.checkValue(null || undefined)).toBeFalsy();
+});
+
+test("check user name object", () => {
+  expect(functions.createUser()).toEqual({ firstName: "Muhannad", lastName: "Judah" });
+});
+
+test("greater than 1600", () => {
+  const a = 1000;
+  const b = 700;
+  expect(a + b).toBeGreaterThan(1600);
+});
+
+test("there in jo i in team", () => {
+  expect("teaM").toMatch(/m/i);
+});
+
+test("array contains Ahmed ", () => {
+  expect(["Muhannad", "Ahmed", "Hana"]).toContainEqual("Ahmed");
+});
+
+test("user fetched data use should be Leanne Graham", async () => {
+  expect.assertions(1);
+  const data = await functions.fetchUser();
+  expect(data.name).toEqual("Leanne Graham");
+});
+
+test("reverse function not undefined", () => {
+  expect(functions.reversString).toBeDefined();
+});
+
+test("reverse Sting", () => {
+  expect(functions.reversString("hello")).toEqual("olleh");
+});
+
+test("reverse Sting (case not sensitive)", () => {
+  expect(functions.reversString("HELLO")).toEqual("olleh");
+});
+
+test("chunk array", () => {
+  expect(functions.chunkArray).toBeDefined();
+});
+
+test("chunk array", () => {
+  expect(functions.chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3)).toEqual([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [10]
+  ]);
+});
+
+test("check if it is function ", () => {
+  expect(typeof functions.isAnagram).toEqual("function");
+});
+
+test("isAnagram ", () => {
+  expect(functions.isAnagram("stet#", "test")).toBeTruthy();
 });
