ebd81ee4b25f1c34bce9895b319a657e02309f3b
muhannad <muhannadjudah@gmail.com>
2020-07-10 22:42:22 +0200

    import bootstrap

--- jest-unit-test/src/js/app.js
+++ jest-unit-test/src/js/app.js
@@ -1,7 +1,11 @@
 require("!style-loader!css-loader!../css/styles.css");
+require("!style-loader!css-loader!bootstrap/dist/css/bootstrap.min.css");
 const arr = require("./script2");
+const { text } = require("./script2");
 const $ = require("jquery");
 
 $.each(arr, (key, value) => {
   $("body").append(`<h4>${arr[key].name}</h4>`);
 });
+
+$("body").append(`<p>${text}</p>`);
--- jest-unit-test/src/js/script2.js
+++ jest-unit-test/src/js/script2.js
@@ -1,7 +1,8 @@
-let text = "this is a domy rext from another file";
+const text = "this is a domy rext from another file";
 const arr = [
   { name: "muhannad", age: 30 },
   { name: "hiba", age: 29 },
   { name: "razan", age: 27 }
 ];
 module.exports = arr;
+module.exports.text = text;
